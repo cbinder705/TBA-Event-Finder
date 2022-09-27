828d2cfc56c1a95e8552a3ecc4333fcfbbf25549
Cory Bee <cbinder705@gmail.com>
2022-09-26 22:33:53 -0400

    added event function for api to javascript

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,21 @@
 
 // Use of the Events API
 
+function events(city) {
+  fetch(
+    "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=KDhnCyJ8Hg2vSizuLF0Xs4WtQFp2wQQo&city=" +
+      city + // user inputs location
+      ""
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+events();
+function DisplayEvents(data) {
+  // links to html
+  const name = data;
+  const description = data.events;
+}
 // Local Storage
 
 // functioning search and defined input buttons
