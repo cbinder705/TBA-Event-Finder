9f069df40c7dae3f775d58d80eccdec756a829ce
Cory Bee <cbinder705@gmail.com>
2022-09-27 21:43:31 -0400

    changed map api parameters and function

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,19 @@
 // initial commit
 
 // Use of the Maps API
-const geocoder = new google.maps.Geocoder();
-
-function initMap() {
-  // The location of Uluru
-  var uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
-}
-submitButton.addEventListener("click", () =>
-  geocode({ address: inputText.value })
-);
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1f20e52452mshd76596b35a6ea62p1e8fecjsnb06971f2d618",
+    "X-RapidAPI-Host": "google-maps28.p.rapidapi.com",
+  },
+};
+var apiURL =
+  "https://google-maps28.p.rapidapi.com/maps/api/place/details/json?fields=address_component%2Cadr_address%2Cbusiness_status%2Cformatted_address%2Cgeometry%2Cicon%2Cicon_mask_base_uri%2Cicon_background_color%2Cname%2Cpermanently_closed%2Cphoto%2Cplace_id%2Cplus_code%2Ctype%2Curl%2Cutc_offset%2Cvicinity%2Cformatted_phone_number%2Cinternational_phone_number%2Copening_hours%2Cwebsite%2Cprice_level%2Crating%2Creview%2Cuser_ratings_total&place_id=ChIJ37HL3ry3t4kRv3YLbdhpWXE&language=en&region=en";
+fetch(apiURL, options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
 
 // Use of the Events API
 
