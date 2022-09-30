b6b7376f2eaed2a16a2d4a3dd794bad253a78626
Cory Bee <cbinder705@gmail.com>
2022-09-29 20:55:35 -0400

    trying to merge changes from main

--- assets/script.js
+++ assets/script.js
@@ -6,21 +6,21 @@
 
 function events(city) {
   fetch(
-    "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=KDhnCyJ8Hg2vSizuLF0Xs4WtQFp2wQQo&city=" +
-      city + // user inputs location
-      ""
+    "https://app.ticketmaster.com/discovery/v2/events.json?&city=" +
+      city + // added string to be able to search for any city
+      "&apikey=25HXWz0Ao4txUIjAm8EGenva5u1M7pt0"
   )
     .then((response) => response.json())
     .then((data) => this.DisplayEvents(data));
 }
 events();
 function DisplayEvents(data) {
-  // links to html
-  const name = data;
-  const type = data.events;
-  console.log(name, type);
+  let location = data; // data from fetch
+  let { name, url } = data.events[0]; // extracting name of event and link to event from json array
+  console.log(location, name, url); // testing purposes
 }
-
+DisplayEvents();
+//
 // Local Storage
 
 // functioning search and defined input buttons
