4d2eab24e7fea2ff3d0ca1081d7ae2d50b05b555
Cory Bee <cbinder705@gmail.com>
2022-10-01 16:17:45 -0400

    fixed bug with event function

--- assets/script.js
+++ assets/script.js
@@ -6,20 +6,18 @@
 
 function events(city) {
   fetch(
-    "https://app.ticketmaster.com/discovery/v2/events.json?&city=" +
-      city + // added string to be able to search for any city
-      "&apikey=25HXWz0Ao4txUIjAm8EGenva5u1M7pt0"
+    "https://app.ticketmaster.com/discovery/v2/events.json?&city=Philadelphia&apikey=25HXWz0Ao4txUIjAm8EGenva5u1M7pt0"
   )
     .then((response) => response.json())
-    .then((data) => this.DisplayEvents(data));
+    .then(DisplayEvents);
 }
 events();
 function DisplayEvents(data) {
-  let location = data; // data from fetch
-  let { name, url } = data.events[0]; // extracting name of event and link to event from json array
-  console.log(location, name, url); // testing purposes
+  let eventName = data._embedded.events[0].name;
+  let eventLink = data._embedded.events[0].url;
+  console.log(eventName, eventLink);
 }
-DisplayEvents();
+
 //
 // Local Storage
 
