59cb2ac3189afcf64119cd82f6670332d11667e0
Cory Bee <cbinder705@gmail.com>
2022-10-02 16:03:54 -0400

    fixed event function

--- assets/script.js
+++ assets/script.js
@@ -1,84 +1,85 @@
 // initial commit
+$.ajax({
+  type: "GET",
+  url: "https://app.ticketmaster.com/discovery/v2/events.json?size=5&city=Camden&apikey=25HXWz0Ao4txUIjAm8EGenva5u1M7pt0",
+  async: true,
+  dataType: "json",
+  success: function (data) {
+    console.log(data._embedded.events[0].name);
+    console.log(data._embedded.events[0].url);
+    // Parse the response.
+    // Do other things.
+  },
+  error: function (xhr, status, err) {
+    // This time, we do not end up here!
+  },
+}),
+  function DisplayEvents(data) {
+    //Need to display on page
+    console.log(data._embedded.events[0].name[0]);
+    let eventLink0 = data._embedded.events[0].url;
+    let eventName1 = data._embedded.events[1].name;
+    let eventLink1 = data._embedded.events[1].url;
+    let eventName2 = data._embedded.events[2].name;
+    let eventLink2 = data._embedded.events[2].url;
+    let eventName3 = data._embedded.events[3].name;
+    let eventLink3 = data._embedded.events[3].url;
+    let eventName4 = data._embedded.events[4].name;
+    let eventLink4 = data._embedded.events[4].url;
 
-// Use of the Maps APIfunction events(location) {
-fetch(
-  "https://app.ticketmaster.com/discovery/v2/events.json?&city=" +
-    location + // added string to be able to search for any city
-    "&apikey=25HXWz0Ao4txUIjAm8EGenva5u1M7pt0"
-)
-  .then((response) => response.json())
-  .then(DisplayEvents)
-  .then(initMap);
-var location = document.getElementById("location").value;
-
-events();
-function DisplayEvents(data) {
-  //Need to display on page
-  let eventName0 = data._embedded.events[0].name[0];
-  let eventLink0 = data._embedded.events[0].url;
-  let eventName1 = data._embedded.events[1].name;
-  let eventLink1 = data._embedded.events[1].url;
-  let eventName2 = data._embedded.events[2].name;
-  let eventLink2 = data._embedded.events[2].url;
-  let eventName3 = data._embedded.events[3].name;
-  let eventLink3 = data._embedded.events[3].url;
-  let eventName4 = data._embedded.events[4].name;
-  let eventLink4 = data._embedded.events[4].url;
-
-  let evtLatitude0 =
-    data._embedded.events[0]._embedded.venues[0].location.latitude;
-  let evtLongitude0 =
-    data._embedded.events[0]._embedded.venues[0].location.longitude;
-  let eventLatitude0 = parseFloat(evtLatitude0);
-  let eventLongitude0 = parseFloat(evtLongitude0);
-  let evtLatitude1 =
-    data._embedded.events[1]._embedded.venues[0].location.latitude;
-  let evtLongitude1 =
-    data._embedded.events[1]._embedded.venues[0].location.longitude;
-  let eventLatitude1 = parseFloat(evtLatitude1);
-  let eventLongitude1 = parseFloat(evtLongitude1);
-  let evtLatitude2 =
-    data._embedded.events[2]._embedded.venues[0].location.latitude;
-  let evtLongitude2 =
-    data._embedded.events[2]._embedded.venues[0].location.longitude;
-  let eventLatitude2 = parseFloat(evtLatitude2);
-  let eventLongitude2 = parseFloat(evtLongitude2);
-  let evtLatitude3 =
-    data._embedded.events[3]._embedded.venues[0].location.latitude;
-  let evtLongitude3 =
-    data._embedded.events[3]._embedded.venues[0].location.longitude;
-  let eventLatitude3 = parseFloat(evtLatitude3);
-  let eventLongitude3 = parseFloat(evtLongitude3);
-  let evtLatitude4 =
-    data._embedded.events[4]._embedded.venues[0].location.latitude;
-  let evtLongitude4 =
-    data._embedded.events[4]._embedded.venues[0].location.longitude;
-  let eventLatitude4 = parseFloat(evtLatitude4);
-  let eventLongitude4 = parseFloat(evtLongitude4);
-  localStorage.setItem("Latitude0", eventLatitude0);
-  localStorage.setItem("Longitude0", eventLongitude0);
-  localStorage.setItem("Latitude1", eventLatitude1);
-  localStorage.setItem("Longitude1", eventLongitude1);
-  localStorage.setItem("Latitude2", eventLatitude2);
-  localStorage.setItem("Longitude2", eventLongitude2);
-  localStorage.setItem("Latitude3", eventLatitude3);
-  localStorage.setItem("Longitude3", eventLongitude3);
-  localStorage.setItem("Latitude4", eventLatitude4);
-  localStorage.setItem("Longitude4", eventLongitude4);
-  localStorage.setItem("Name0", eventName0);
-  localStorage.setItem("Link0", eventLink0);
-  localStorage.setItem("Name1", eventName1);
-  localStorage.setItem("Link1", eventLink1);
-  localStorage.setItem("Name2", eventName2);
-  localStorage.setItem("Link2", eventLink2);
-  localStorage.setItem("Name3", eventName3);
-  localStorage.setItem("Link3", eventLink3);
-  localStorage.setItem("Name4", eventName4);
-  localStorage.setItem("Link4", eventLink4);
-  localStorage.setItem("Name5", eventName5);
-  localStorage.setItem("Link5", eventLink5);
-}
-DisplayEvents();
+    let evtLatitude0 =
+      data._embedded.events[0]._embedded.venues[0].location.latitude;
+    let evtLongitude0 =
+      data._embedded.events[0]._embedded.venues[0].location.longitude;
+    let eventLatitude0 = parseFloat(evtLatitude0);
+    let eventLongitude0 = parseFloat(evtLongitude0);
+    let evtLatitude1 =
+      data._embedded.events[1]._embedded.venues[0].location.latitude;
+    let evtLongitude1 =
+      data._embedded.events[1]._embedded.venues[0].location.longitude;
+    let eventLatitude1 = parseFloat(evtLatitude1);
+    let eventLongitude1 = parseFloat(evtLongitude1);
+    let evtLatitude2 =
+      data._embedded.events[2]._embedded.venues[0].location.latitude;
+    let evtLongitude2 =
+      data._embedded.events[2]._embedded.venues[0].location.longitude;
+    let eventLatitude2 = parseFloat(evtLatitude2);
+    let eventLongitude2 = parseFloat(evtLongitude2);
+    let evtLatitude3 =
+      data._embedded.events[3]._embedded.venues[0].location.latitude;
+    let evtLongitude3 =
+      data._embedded.events[3]._embedded.venues[0].location.longitude;
+    let eventLatitude3 = parseFloat(evtLatitude3);
+    let eventLongitude3 = parseFloat(evtLongitude3);
+    let evtLatitude4 =
+      data._embedded.events[4]._embedded.venues[0].location.latitude;
+    let evtLongitude4 =
+      data._embedded.events[4]._embedded.venues[0].location.longitude;
+    let eventLatitude4 = parseFloat(evtLatitude4);
+    let eventLongitude4 = parseFloat(evtLongitude4);
+    localStorage.setItem("Latitude0", eventLatitude0);
+    localStorage.setItem("Longitude0", eventLongitude0);
+    localStorage.setItem("Latitude1", eventLatitude1);
+    localStorage.setItem("Longitude1", eventLongitude1);
+    localStorage.setItem("Latitude2", eventLatitude2);
+    localStorage.setItem("Longitude2", eventLongitude2);
+    localStorage.setItem("Latitude3", eventLatitude3);
+    localStorage.setItem("Longitude3", eventLongitude3);
+    localStorage.setItem("Latitude4", eventLatitude4);
+    localStorage.setItem("Longitude4", eventLongitude4);
+    localStorage.setItem("Name0", eventName0);
+    localStorage.setItem("Link0", eventLink0);
+    localStorage.setItem("Name1", eventName1);
+    localStorage.setItem("Link1", eventLink1);
+    localStorage.setItem("Name2", eventName2);
+    localStorage.setItem("Link2", eventLink2);
+    localStorage.setItem("Name3", eventName3);
+    localStorage.setItem("Link3", eventLink3);
+    localStorage.setItem("Name4", eventName4);
+    localStorage.setItem("Link4", eventLink4);
+    localStorage.setItem("Name5", eventName5);
+    localStorage.setItem("Link5", eventLink5);
+  };
 // Instantiate a map and platform object:
 function initMap() {
   // The location of Uluru
